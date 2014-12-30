8a9b38d3a4f219b78747868f07dbb4040ff5b71d
Leonid Rozenblyum <lrozenblyum@gmail.com>
2014-12-30 17:39:08 +0200

    Namespace prepared.

---
+++ richHebrew/src/richHebrew.js
@@ -0,0 +1,6 @@
+/**
+ * Rich possibilities to type and style Hebrew text in html
+ */
+if ( typeof com === "undefined" ) { com = {}; }
+if ( typeof com.leokom === "undefined" ) { com.leokom = {}; }
+if ( typeof com.leokom.richHebrew === "undefined" ) { com.leokom.richHebrew = {}; } 
